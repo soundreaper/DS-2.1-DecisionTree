4582505ad6c0dfa6de3c8850edb05b2a0bafb394
soundreaper <soundreaper@gmail.com>
2020-05-13 09:50:17 -0700

    remove old files and add relavent conent for deicsion tree's to flask application, also add generated tree visualization

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask, render_template, url_for, send_from_directory
+from flask import Flask, render_template, url_for, send_from_directory, send_file, request
 
 app = Flask(__name__)
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'img/mypage.jpg', mimetype='image/vnd.microsoft.icon')
+                               'img/favicon.png', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def index():
